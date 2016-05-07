b1ee983ee6f712cded50376b87c1b49e0ed1c3a8
sgim <levin.ab@gmail.com>
2016-05-07 19:05:10 -0400

    fixed camera on portals

---
+++ UnityFiles/Assets/Scripts/portalUnity_fromonline/PortalGun.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 
+ * Released under the creative commons attribution license.
+ * Do whatever you like with the code, just give credit to Stuart Spence.
+ * https://creativecommons.org/licenses/by/3.0/
+ */ 
+
+public class PortalGun : MonoBehaviour
+{
+    public float gunRange = 1000;
+    public AudioClip shootSound, teleportSound;
+    private Portal[] portals;
+
+    void Start()
+    {
+        portals = GameObject.FindObjectsOfType<Portal>();
+        if (portals.Length != 2)
+            Debug.LogError("You need to attach the Portal script to exactly two gameobjects in the world.");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Vector3 position = Camera.main.transform.position;
+            RaycastHit rayCastHit = new RaycastHit();
+            if (Physics.Linecast(position, position + Camera.main.transform.forward * gunRange, out rayCastHit, 1))
+            {
+                int index = Input.GetMouseButtonDown(0) ? 0 : 1;
+                portals[index].MovePortal(rayCastHit);
+            }
+        }
+    }
+}
