92b07063d69b0373347a2dde20e5353b986ac6e0
sgim <levin.ab@gmail.com>
2016-05-09 18:00:41 -0400

    progress on platforms level and implemented fast moving platforms

---
+++ UnityFiles/Assets/Scripts/Platform/SpringingPlatform.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringingPlatform : MonoBehaviour {
+
+	//alternate way to make variable public - acts differently?
+	[SerializeField]
+	Transform platform;
+
+	[SerializeField]
+	Transform startTransform;
+
+	[SerializeField]
+	Transform endTransform;
+
+	[SerializeField]
+	float platformSpeed;
+
+	Vector3 direction;
+	Transform destination;
+
+	void Start(){
+		SetDestination (startTransform);
+	}
+
+	void FixedUpdate() {
+		float timeSpeed = platformSpeed * Time.fixedDeltaTime;
+//		platform.GetComponent<Rigidbody>().MovePosition (platform.position + direction * timeSpeed);
+		// alternative using Lerp - which moves from one position to another.
+		// much jerkier, but would be interesting as a different mechanic
+		platform.position = Vector3.Slerp(platform.position, destination.position, timeSpeed);
+
+		if (Vector3.Distance (platform.position, destination.position) < timeSpeed) {
+			SetDestination (destination == startTransform ? endTransform : startTransform);
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube (startTransform.position, platform.localScale);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube (endTransform.position, platform.localScale);
+	}
+
+	void SetDestination(Transform dest) {
+		destination = dest;
+		direction = (destination.position - platform.position).normalized;
+	}
+
+}
