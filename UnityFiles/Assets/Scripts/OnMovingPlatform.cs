880d0307c084c1e9cda09a42073635d3e64b678f
sgim <levin.ab@gmail.com>
2016-05-09 14:32:10 -0400

    beginning making of movingplatform level

--- UnityFiles/Assets/Scripts/OnMovingPlatform.cs
+++ UnityFiles/Assets/Scripts/OnMovingPlatform.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class OnMovingPlatform : MonoBehaviour {
 
 	private Rigidbody rb;
+	private UnityEngine.Transform platform;
+	private Vector3 platformLocalPoint;
+	private Vector3 platformGlobalPoint;
+	private Vector3 platformLastVelocity;
+
+	private Quaternion platformLocalRotation;
+	private Quaternion platformGlobalRotation;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -14,6 +22,37 @@ public class OnMovingPlatform : MonoBehaviour {
 		if ((transform.parent || rb.isKinematic) && (Input.GetAxis ("Vertical") != 0|| Input.GetAxis ("Horizontal") != 0|| Input.GetKeyDown (KeyCode.Space))) {
 			rb.isKinematic = false;
 			transform.parent = null;
+		}
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.collider.tag == "Platform") {
+			
+			Debug.Log ("on a platform!");
+			platform = col.collider.transform;
+
+			//rb.isKinematic = true;
+//			transform.parent = col.collider.transform;
+		}
+	}
+
+	void OnCollisionStay(Collision col) {
+		if (col.collider.tag == "Platform") {
+			platformGlobalPoint = transform.position;
+			platformLocalPoint = col.collider.transform.InverseTransformPoint (transform.position);
+			Vector3 newPoint = platform.TransformPoint (platformLocalPoint);
+			Vector3 moveDistance = newPoint - platformGlobalPoint;
+			Debug.Log (moveDistance);
+//			platformLastVelocity = (newPoint - platformGlobalPoint) / Time.deltaTime;
+//			col.collider.transform.TransformPoint (col.collider);
+			//rb.isKinematic = true;
+
+		}
+	}
+
+	void OnCollisionExit(Collision col) {
+		if (col.collider.tag == "Platform") {
+			//rb.isKinematic = false;
 		}
 	}
 
