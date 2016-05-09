75cf1426e0f504582b43b4dcddcbde00ebcc71e7
cindyliu686 <cindyliuyn@gmail.com>
2016-05-09 12:04:13 -0400

    menu

---
+++ UnityFiles/Assets/Scripts/menuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+	public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+	
+	public void ExitPress()
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress()
+	{
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene (nextScene);;   // load Scene
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
