52e4c47a0622765e0d95ec740ba8f537df923047
sgim <levin.ab@gmail.com>
2016-05-10 17:18:51 -0400

    had to revert back to old onplatform function

--- UnityFiles/Assets/Scripts/Player/OnMovingPlatform.cs
+++ UnityFiles/Assets/Scripts/Player/OnMovingPlatform.cs
@@ -3,29 +3,26 @@ using System.Collections;
 
 public class OnMovingPlatform : MonoBehaviour {
 
+	private Rigidbody rb;
 	private UnityEngine.Transform platform;
+	private Vector3 platformLocalPoint;
+	private Vector3 platformGlobalPoint;
 	private bool onPlatform = false;
-	private Vector3 platformOldPosition;
-	private Vector3 platformNewPosition;
-
-	void Start () {
-
-	}
 
 	void FixedUpdate () {
 		if (onPlatform) {
-			platformNewPosition = platform.position;
-			transform.position += platformNewPosition - platformOldPosition;
-			platformOldPosition = platformNewPosition;
+			transform.position += platform.TransformPoint (platformLocalPoint) - platformGlobalPoint;;
+			platformGlobalPoint = transform.position;
+			platformLocalPoint = platform.InverseTransformPoint (transform.position);
 		}
 	}
 
-	// This needs work!!!
 	void OnCollisionEnter(Collision col) {
 		if (col.collider.tag == "Platform") {
 			onPlatform = true;
 			platform = col.collider.transform;
-			platformOldPosition = platformNewPosition = platform.position;
+			platformGlobalPoint = transform.position;
+			platformLocalPoint = platform.InverseTransformPoint (transform.position);
 		}
 	}
 
