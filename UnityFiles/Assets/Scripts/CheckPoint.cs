98c758177c003a45b1b03346d9a2ec96225405ef
cindyliu686 <cindyliuyn@gmail.com>
2016-05-10 18:30:11 -0400

    add pauseMenu

--- UnityFiles/Assets/Scripts/CheckPoint.cs
+++ UnityFiles/Assets/Scripts/CheckPoint.cs
@@ -5,35 +5,10 @@ using System.Runtime.Serialization.Formatters.Binary;
 public class CheckPoint : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
-		// save the player into file
-		BinaryFormatter bf = new BinaryFormatter ();
-		FileStream file = File.Create (Application.persistentDataPath + "/playerInfo.dat");
-		PlayerData data = new PlayerData ();
-		data.health = health;
-		data.experience = experience;
-
-		bf.Serialize (file, data);
-		file.Close ();
-
 		// also set the respawn position to this checkpoint position;
 		PlayerHealth.respawnPoint = transform.position;
 		
 	}
 		
-	public void Load(){
-		if (File.Exists (Application.persistentDataPath + "/playerInfo.dat")) {
-			BinaryFormatter bf = new BinaryFormatter ();
-			FileStream file = File.open (Application.persistentDataPath + "/playerInfo.dat");
-			PlayerData data = (PlayerData)bf.Deserialize (file);
-			file.Close ();
-
-
-		}
-	}
-		
 }
-
-[Serializable]
-class PlayerData (){
-	public int health = PlayerHealth.currentHealth;
-}
+	
--- UnityFiles/Assets/Scripts/ExitAndSave.cs
+++ UnityFiles/Assets/Scripts/ExitAndSave.cs
@@ -1,7 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ExitAndSave : MonoBehaviour {
+	public Transform Player;
+
+	public void SaveGameSettings(bool Quit)
+	{
+		PlayerPrefs.SetFloat ("x",Player.position.x);
+		PlayerPrefs.SetFloat ("y",Player.position.y);
+		PlayerPrefs.SetFloat ("z",Player.position.z);
+		PlayerPrefs.SetFloat ("Cam_y", Player.eulerAngles.y);
+		if (Quit) {
+			Time.timeScale = 1;
+			SceneManager.LoadScene ("Menu");
+		}
+	}
 
 	// save the player into file
 	//		BinaryFormatter bf = new BinaryFormatter ();
