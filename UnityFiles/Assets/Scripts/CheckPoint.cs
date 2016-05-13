6a5e9305c2d079207e86ba59218967327bef5614
iMoe037 <i.mohamed037@gmail.com>
2016-05-13 15:25:44 -0400

    Checkpoint and triggers Work

--- UnityFiles/Assets/Scripts/CheckPoint.cs
+++ UnityFiles/Assets/Scripts/CheckPoint.cs
@@ -7,59 +7,66 @@ public class CheckPoint : MonoBehaviour {
 	public Transform Player;
 	public bool enterOnce = true;
 	public float showDuration = 3.0f;
+	public bool isCheckpoint = false;
+	public string txtmessage = "Enter Text";
+	public GUIStyle textStyle;
 
 	private bool alreadyEntered = false;
 	private float enteredTime;
 	private float currentTime;
-	private GameObject checkpoint;
+	private bool showMsg = false;
 //	PlayerHealth PlayerHealth;
 
 	void Start(){
-//		checkpoint = GameObject.Find ("Checkpoint");
-//		this.gameObject.SetActive (false);
-//		Debug.Log();
-//		checkpoint.GetComponent<Text> ().enabled = false;
-//		PlayerHealth = Player.GetComponent<PlayerHealth> ();
-//		Debug.Log(this.gameObject);
+		if (isCheckpoint) {
+			txtmessage = "Checkpoint!";
+		}
 	}
 
 	void Update() {
 		currentTime = Time.time;
 
 		if (currentTime - enteredTime > showDuration) {
-//			checkpoint.SetActive (false);
-//			this.gameObject.SetActive (false);
+			showMsg = false;
+		}
+	}
+
+	void OnGUI() {
+		if (showMsg) {
+			GUI.contentColor = Color.red;
+			GUI.Label (new Rect (Screen.width/2-50, 20, 100, 50), txtmessage, textStyle);
 		}
 	}
 
 	void OnTriggerEnter(Collider other){
-		Debug.Log ("Entered");
-		if (!alreadyEntered) {
+		if (!alreadyEntered || !enterOnce) {
 			//Start timer and show Text
 			enteredTime = Time.time;
+			showMsg = true;
 
-			// also update the respawn position to this checkpoint position;
-			PlayerHealth.respawnPoint = transform.position;
-			PlayerHealth.respawnPointAngle = transform.eulerAngles;
+			if(isCheckpoint) {
+				
+				// also update the respawn position to this checkpoint position;
+				PlayerHealth.respawnPoint = transform.position;
+				PlayerHealth.respawnPointAngle = transform.eulerAngles;
 
-			// save all the player info at the checkpoint to PlayerPrefs;
-			//position
-			PlayerPrefs.SetFloat ("x",Player.position.x);
-			PlayerPrefs.SetFloat ("y",Player.position.y);
-			PlayerPrefs.SetFloat ("z",Player.position.z);
-			PlayerPrefs.SetFloat ("Cam_y", Player.eulerAngles.y);
-			//health
-			//		PlayerPrefs.SetInt ("currentHealth", PlayerHealth.currentHealth);
-			//respawnPoint
-			PlayerPrefs.SetFloat ("RPx", PlayerHealth.respawnPoint.x);
-			PlayerPrefs.SetFloat ("RPy", PlayerHealth.respawnPoint.y);
-			PlayerPrefs.SetFloat ("RPz", PlayerHealth.respawnPoint.z);
-			PlayerPrefs.SetFloat ("RPA_y", PlayerHealth.respawnPointAngle.y);
-			// stamina
+				// save all the player info at the checkpoint to PlayerPrefs;
+				//position
+				PlayerPrefs.SetFloat ("x",Player.position.x);
+				PlayerPrefs.SetFloat ("y",Player.position.y);
+				PlayerPrefs.SetFloat ("z",Player.position.z);
+				PlayerPrefs.SetFloat ("Cam_y", Player.eulerAngles.y);
+				//health
+				//		PlayerPrefs.SetInt ("currentHealth", PlayerHealth.currentHealth);
+				//respawnPoint
+				PlayerPrefs.SetFloat ("RPx", PlayerHealth.respawnPoint.x);
+				PlayerPrefs.SetFloat ("RPy", PlayerHealth.respawnPoint.y);
+				PlayerPrefs.SetFloat ("RPz", PlayerHealth.respawnPoint.z);
+				PlayerPrefs.SetFloat ("RPA_y", PlayerHealth.respawnPointAngle.y);
+				// stamina
+			};
 
-			Debug.Log (alreadyEntered);
 			alreadyEntered = true;
-			Debug.Log (alreadyEntered);
 		}
 	}
 		
