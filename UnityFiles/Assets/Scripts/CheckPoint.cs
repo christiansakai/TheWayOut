9f6baa84e94ea2d073581ba1564ad8c7a87b29da
iMoe037 <i.mohamed037@gmail.com>
2016-05-13 17:27:49 -0400

    Fixed a bug in messages

--- UnityFiles/Assets/Scripts/CheckPoint.cs
+++ UnityFiles/Assets/Scripts/CheckPoint.cs
@@ -9,15 +9,20 @@ public class CheckPoint : MonoBehaviour {
 	public float showDuration = 3.0f;
 	public bool isCheckpoint = false;
 	public string txtmessage = "Enter Text";
-	public GUIStyle textStyle;
+//	public GUIStyle textStyle;
 
 	private bool alreadyEntered = false;
 	private float enteredTime;
 	private float currentTime;
 	private bool showMsg = false;
+	private bool created = false;
+	private bool isCalled = false;
+	private Text currentMsg;
 //	PlayerHealth PlayerHealth;
 
 	void Start(){
+
+		currentMsg = GameObject.Find ("MessageCenter").GetComponent<Text>();
 		if (isCheckpoint) {
 			txtmessage = "Checkpoint!";
 		}
@@ -26,23 +31,30 @@ public class CheckPoint : MonoBehaviour {
 	void Update() {
 		currentTime = Time.time;
 
-		if (currentTime - enteredTime > showDuration) {
-			showMsg = false;
+		if (currentTime - enteredTime > showDuration && !isCalled) {
+			isCalled = true;
+			currentMsg.text = "";
 		}
 	}
 
-	void OnGUI() {
-		if (showMsg) {
-			GUI.contentColor = Color.red;
-			GUI.Label (new Rect (Screen.width/2-50, 20, 100, 50), txtmessage, textStyle);
-		}
-	}
+//	void OnGUI() {
+//		if (showMsg) {
+//			if (!created) {
+//				GUI.Label (new Rect (Screen.width / 2 - 50, 20, 100, 50), txtmessage, textStyle);
+//			} else {
+//				Debug.Log ("Other Text");
+////				GUI.Label (new Rect (Screen.width / 2 - 50, 20, 100, 50), txtmessage, textStyle);
+//			}
+//
+//		}
+//	}
 
 	void OnTriggerEnter(Collider other){
 		if (!alreadyEntered || !enterOnce) {
 			//Start timer and show Text
 			enteredTime = Time.time;
-			showMsg = true;
+			currentMsg.text = txtmessage;
+//			showMsg = true;
 
 			if(isCheckpoint) {
 				
