dabe3e5c93ee450cdb5ecb34fee25b63804fb898
iMoe037 <i.mohamed037@gmail.com>
2016-05-14 16:50:07 -0400

    Player, Remote, Portals Work Still Buggy

---
+++ UnityFiles/Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public float forwardSpeed = 5.0f;
+	public float strafeSpeed = 4.0f;
+	public float mouseSensitivity = 2.0f;
+	public float clampLook = 86.0f;
+	public float jumpHeight = 7.0f;
+	public bool invertControls = false;
+	public float joyStickXSensitivity = 2.0f;
+	public float joyStickYSensitivity = 2.0f;
+
+	float rotY = 0;
+	float rotX;
+	float joyRotY;
+	float joyRotX;
+	float vertical;
+	float horizontal;
+	float verticalVelocity = 0;
+
+	CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Rotation
+		rotX = Input.GetAxis ("JoyStick X") * joyStickXSensitivity;
+		rotX += Input.GetAxis("Mouse X") * mouseSensitivity;
+		rotY += Input.GetAxis ("JoyStick Y") * joyStickYSensitivity;
+		rotY -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+
+
+		transform.Rotate (0, rotX, 0);
+
+		rotY = Mathf.Clamp (rotY, -clampLook, clampLook);
+		Camera.main.transform.localRotation = Quaternion.Euler (rotY, 0, 0);
+		
+		//Movement
+		vertical = Input.GetAxis ("Vertical") * forwardSpeed;
+		horizontal = Input.GetAxis ("Horizontal") * strafeSpeed;
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+		Vector3 speed = new Vector3 (horizontal, verticalVelocity, vertical);
+		 
+		if (characterController.isGrounded && Input.GetButtonDown ("Jump")) {
+			verticalVelocity = jumpHeight;
+		}
+		speed = transform.rotation * speed;
+
+		characterController.Move (speed * Time.deltaTime);
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit other) {
+		Debug.Log ("Entered");
+		if(other.collider.tag == "Portal") {
+			GameObject portal = other.collider.GetComponent<StepThroughPortal> ().otherPortal;
+			transform.position = portal.transform.position + portal.transform.forward * 3;
+		}
+	}
+}
--- UnityFiles/Assets/Scripts/StepThroughPortal.cs
+++ UnityFiles/Assets/Scripts/StepThroughPortal.cs
@@ -3,18 +3,17 @@ using System.Collections;
 
 public class StepThroughPortal : MonoBehaviour {
 	public GameObject otherPortal;
-	private UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController player;
+	private GameObject player;
 	private Rigidbody rb;
 
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>();
+		player = GameObject.FindGameObjectWithTag ("Player");
 		rb = player.GetComponent<Rigidbody> ();
 	}
 
-	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Player") {
+	void OnControllerColliderHit(ControllerColliderHit other) {
+		Debug.Log ("Entered");
 			
-
 //			rb.velocity = otherPortal.transform.forward * rb.velocity.magnitude;
 
 //			rb = other.attachedRigidbody;
@@ -23,20 +22,18 @@ public class StepThroughPortal : MonoBehaviour {
 //			rb.isKinematic = false;
 //
 //			Rigidbody rb = other.attachedRigidbody;
-			Vector3 vel = rb.velocity + rb.angularVelocity;
-			vel = Vector3.Reflect (vel, transform.forward);		
-			vel = transform.InverseTransformDirection (vel);		
-			vel = otherPortal.transform.TransformDirection (vel);		
-				
-			rb.rotation = otherPortal.transform.rotation;		
-			rb.position = otherPortal.transform.position + otherPortal.transform.forward * 3;
-			rb.AddForce ((otherPortal.transform.position - player.transform.position).normalized * 20 * Time.smoothDeltaTime);
+//			Vector3 vel = rb.velocity + rb.angularVelocity;
+//			vel = Vector3.Reflect (vel, transform.forward);		
+//			vel = transform.InverseTransformDirection (vel);		
+//			vel = otherPortal.transform.TransformDirection (vel);		
+//			rb.rotation = otherPortal.transform.rotation;		
+			player.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 3;
+//			rb.AddForce ((otherPortal.transform.position - player.transform.position).normalized * 20 * Time.smoothDeltaTime);
 //			rb.velocity = vel * rb.velocity.magnitude;
 
 
 //			other.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 1;
 
 
-		}
 	}
 }
