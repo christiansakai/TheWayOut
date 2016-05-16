bac0f1a6b59031316eda80d8cb152a1c4e3d0568
iMoe037 <i.mohamed037@gmail.com>
2016-05-16 11:22:06 -0400

    Bin Files

--- UnityFiles/Assets/GravityLift.cs
+++ UnityFiles/Assets/GravityLift.cs
@@ -4,31 +4,4 @@ using System.Collections;
 public class GravityLift : MonoBehaviour {
 
 	public float liftForce = 40;
-	private GameObject player;
-	private PlayerControls controls;
-	private float runMultipler;
-	private Rigidbody rb;
-
-	void Start () {
-		player = GameObject.FindWithTag ("Player");
-		controls = player.GetComponent<PlayerControls>();
-		rb = player.GetComponent<Rigidbody> ();
-		runMultipler = controls.runMultipler;
-
-	}
-
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.CompareTag ("Player")) {
-			Vector3 currentSpeed = rb.velocity;
-
-			//Still an issue with running towards a gravity lift. Does not launch the player up.
-			if (controls.isRunning) {
-				rb.velocity = new Vector3(currentSpeed.x/runMultipler, currentSpeed.y + liftForce, currentSpeed.z);
-			}
-			else {
-				rb.velocity = new Vector3(currentSpeed.x, currentSpeed.y + liftForce, currentSpeed.z);
-			}
-		}
-	}
 }
