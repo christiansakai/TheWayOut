525a12c79093e6320d46c7bef12419f1342e69d9
sgim <levin.ab@gmail.com>
2016-05-21 12:54:38 -0400

    slightly modified endPlatform to work with state file

--- UnityFiles/Assets/Scripts/Platform/EndPlatformBehavior.cs
+++ UnityFiles/Assets/Scripts/Platform/EndPlatformBehavior.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class EndPlatformBehavior : MonoBehaviour {
 
 	public string nextScene;
+	private State state;
+
+	void Start(){
+		state = GameObject.Find ("GameState").GetComponent<State> ();
+	}
 
 	void OnTriggerEnter(Collider other) {
-		DontDestroyOnLoad (GameObject.Find("GameState").transform.gameObject);
-		UnityEngine.SceneManagement.SceneManager.LoadScene (nextScene);
+		state.LoadScene (nextScene);
 	}
 }
