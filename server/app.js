49a27ffd18c1c73dfc72cdc953b6c2dae853cc6e
sgim <levin.ab@gmail.com>
2016-05-23 11:56:55 -0400

    added quick console test

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./authentication")(app);
+app.use((req, res, next) => (console.log("running"), next()));
 // api routes
 app.use("/api", require("./routes"));
 
@@ -23,3 +24,4 @@ app.use("/api", require("./routes"));
 app.use((err, req, res, next) => console.error(err, err.stack));
 
 app.listen(port, () => console.log("listening on port:", port));
+
