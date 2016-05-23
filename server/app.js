237d5f6803ac6df9bea664a01a89c363d0305f5e
sgim <levin.ab@gmail.com>
2016-05-23 12:13:53 -0400

    looking at process port

--- server/app.js
+++ server/app.js
@@ -8,14 +8,14 @@ var cookieParser = require("cookie-parser");
 var mongoose = require("mongoose");
 require("./models");
 
-var port = 1337;
+var port = process.env.PORT || 1337;
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./authentication")(app);
 app.use((req, res, next) => (console.log("running"), next()));
+require("./authentication")(app);
 // api routes
 app.use("/api", require("./routes"));
 
