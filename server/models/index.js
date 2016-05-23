d510bb077feba6359f0a2997afabaa8eb9b45b0d
sgim <levin.ab@gmail.com>
2016-05-23 12:01:07 -0400

    added mongolog

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var mongodb = process.env.NODE_ENV === "production" ? process.env.MONGOLAB_URI : "mongodb://localhost/unityportal";
+console.log("MONGODB IS: ", mongodb);
 mongoose.connect(mongodb);
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
